81316dfd5f83fc1967b4a5a44bd8f7e0155ed4cc
Victor Kozub <vkozub4@gmail.com>
2013-12-06 19:25:18 +0300

    some style changes

--- lab3/client.rb
+++ lab3/client.rb
@@ -1,27 +1,32 @@
 require 'socket'
-require '../libs/NET_UTILS.rb'
+require '../libs/NetUtils.rb'
 
 hostname = ARGV[0] || '127.0.0.1'
 port = ARGV[1] || 4455
 filename = ARGV[2] || '../out84mb.pdf'
-BUFFER_SIZE = 10
+BUFFER_SIZE = 102
 
-socket = NET_UTILS::TCPClient.new(port, hostname)
+socket = NetUtils::TCPClient.new(port, hostname)
+
+puts "File transfer started."
 
 begin
-  file = File.open(filename, 'w')
-
-  while data = socket.read(BUFFER_SIZE) do
+  File.open(filename, 'w') do |file|
+    while data = socket.read(BUFFER_SIZE) do
       file.write(data)
+    end
+    file.close
+    puts "File transfer completed."
+    break
   end
-  
+ 
+rescue SystemExit, Interrupt => e 
+  puts puts "\nFile transfer was interrupted by user."
+  File.delete(filename)
 rescue
   puts "File transfer failed!"
   File.delete(filename)
-rescue Interrupt
-  puts puts "\nFile transfer was interrupted by user."
-  File.delete(filename)
 ensure
-  file.close
   socket.close
 end
+
--- lab3/server.rb
+++ lab3/server.rb
@@ -1,34 +1,42 @@
 require 'socket'
 require 'benchmark'
-require '../libs/NET_UTILS.rb'
+require '../libs/NetUtils.rb'
 
 hostname = ARGV[0] || '127.0.0.1' 
 port = ARGV[1] || 4455
 filename = ARGV[2] || '../in84mb.pdf'
+file_size = File.size(filename)
 aMB = 1024 * 1024
 BUFFER_SIZE = aMB
 
-socket = NET_UTILS::TCPServer.new(port, hostname)
+socket = NetUtils::TCPServer.new(port, hostname)
 puts "Server is running on #{hostname}:#{port}. Awaiting connections."
 
-client_socket, client_addrinfo = socket.accept
+begin
+  client_socket, client_addrinfo = socket.accept
+rescue SystemExit, Interrupt => e
+  puts "\nServer is shutting down."
+  socket.close
+  exit
+end
 
 puts "File transfer started."
 begin
   time = Benchmark.realtime do
-    file = File.open(filename, 'rb')
-    while data = file.read(BUFFER_SIZE) do
+    File.open(filename, 'rb') do |file|
+      while data = file.read(BUFFER_SIZE) do
         client_socket.write(data)
+      end
+    file.close
     end
   end
   
-  file_size = File.size(filename) / aMB
   puts "File transfer completed. Time elapsed: #{time} sec. Transfered #{file_size} MB of data. Average speed: #{file_size / time} MB/sec"
 
+rescue SystemExit, Interrupt => e
+  puts puts "\nFile transfer was interrupted by user."
 rescue
   puts "File transfer failed!"
-rescue Interrupt
-  puts puts "\nFile transfer was interrupted by user."
 ensure
   client_socket.close
   socket.close
